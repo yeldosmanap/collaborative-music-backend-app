430d08050d978e04a50b65ad41d2e19127eecee1
Yeldos Manap <yeldos.manap@gmail.com>
2023-08-23 01:00:41 +0600

    Add User Entity to Domain

---
+++ CollaborativeMusicApp/CollaborativeMusicApp.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using CollaborativeMusicApp.Domain.Common;
+
+namespace CollaborativeMusicApp.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string Email { get; set; }
+    public string Username { get; set; }
+    [JsonIgnore]
+    public string Password { get; set; }
+}
