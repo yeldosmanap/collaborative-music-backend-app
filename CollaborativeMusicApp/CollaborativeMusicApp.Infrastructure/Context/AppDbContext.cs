01bac2adedabaea9daa27a72645711018fff2344
Yeldos Manap <yeldos.manap@gmail.com>
2023-08-31 12:31:08 +0600

    Add DbContext and Repositories for Auth

---
+++ CollaborativeMusicApp/CollaborativeMusicApp.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CollaborativeMusicApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollaborativeMusicApp.Infrastructure.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
