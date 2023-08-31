9c3080d3ffcd4279fff93102c4519a83813936c8
Yeldos Manap <yeldos.manap@gmail.com>
2023-08-31 14:49:29 +0600

    Refactor Auth Service implementation

--- CollaborativeMusicApp/CollaborativeMusicApp.Application/Services/Implementations/AuthService.cs
+++ CollaborativeMusicApp/CollaborativeMusicApp.Application/Services/Implementations/AuthService.cs
@@ -1,19 +1,26 @@
-﻿using CollaborativeMusicApp.Application.Contract.Persistence;
+﻿using System.Security.Authentication;
+using CollaborativeMusicApp.Application.Common;
+using CollaborativeMusicApp.Application.Contract.Persistence;
 using CollaborativeMusicApp.Application.Exceptions;
 using CollaborativeMusicApp.Domain.DTOs;
 using CollaborativeMusicApp.Domain.DTOs.Login;
 using CollaborativeMusicApp.Domain.DTOs.Register;
 using CollaborativeMusicApp.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace CollaborativeMusicApp.Application.Services.Implementations;
 
 public class AuthService : IAuthService
 {
     private readonly IUserRepository _userRepository;
-
-    public AuthService(IUserRepository userRepository)
+    private readonly IPasswordHasher<User> _passwordHasher;
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    
+    public AuthService(IUserRepository userRepository, IPasswordHasher<User> passwordHasher, IJwtTokenGenerator jwtTokenGenerator)
     {
         _userRepository = userRepository;
+        _passwordHasher = passwordHasher;
+        _jwtTokenGenerator = jwtTokenGenerator;
     }
 
     public async Task<RegisterUserResponseDto> RegisterAsync(RegisterUserRequestDto registerUserRequestDto)
@@ -23,21 +30,52 @@ public class AuthService : IAuthService
         {
             throw new UserAlreadyExistsByEmailException("User already exists with this email");
         }
-        
+
         User user = new()
         {
-            Email = registerUserRequestDto.Username,
+            Email = registerUserRequestDto.Email,
             Password = registerUserRequestDto.Password,
             Username = registerUserRequestDto.Username
         };
+        
+        user.Password = _passwordHasher.HashPassword(user, user.Password);
         
         await _userRepository.AddAsync(user);
         
         return new RegisterUserResponseDto(user.Email, user.Username, true, "");
     }
 
-    public Task<LoginUserResponseDto> LoginAsync(LoginUserRequestDto loginUserRequestDto)
+    public async Task<LoginUserResponseDto> LoginAsync(LoginUserRequestDto loginUserRequestDto)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(loginUserRequestDto.Email) || string.IsNullOrEmpty(loginUserRequestDto.Password))
+        {
+            throw new Exception("Email and password are required");
+        }
+
+        // 1. Get a user by email
+        var user = await _userRepository.GetUserByEmailAsync(loginUserRequestDto.Email);
+        if (user == null)
+        {
+            throw new Exception("User does not exist");
+        }
+
+        // 2. Validate the password
+        var passwordVerificationResult = _passwordHasher.VerifyHashedPassword(user, user.Password,
+            loginUserRequestDto.Password);
+        if (passwordVerificationResult != PasswordVerificationResult.Success)
+        {
+            throw new InvalidCredentialException("Invalid password");
+        }
+
+        // 3. Generate the token
+        var token = _jwtTokenGenerator.GenerateToken(user.Id, user.Username, user.Email);
+
+        return new LoginUserResponseDto(
+            user.Email,
+            user.Username,
+            token,
+            true,
+            ""
+        );
     }
 }
