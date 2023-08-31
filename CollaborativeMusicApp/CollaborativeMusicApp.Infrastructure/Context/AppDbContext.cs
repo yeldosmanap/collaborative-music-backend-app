b7554c070094998c20b3f7c3c2cbd6e3a62d3feb
Yeldos Manap <yeldos.manap@gmail.com>
2023-08-31 14:48:26 +0600

    Refactore AppDbContext

--- CollaborativeMusicApp/CollaborativeMusicApp.Infrastructure/Context/AppDbContext.cs
+++ CollaborativeMusicApp/CollaborativeMusicApp.Infrastructure/Context/AppDbContext.cs
@@ -1,24 +1,34 @@
 ﻿using CollaborativeMusicApp.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace CollaborativeMusicApp.Infrastructure.Context;
 
 public class AppDbContext : DbContext
 {
+    private readonly IConfiguration _configuration;
+    
     public DbSet<User> Users { get; set; } = null!;
 
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    public AppDbContext(IConfiguration configuration)
     {
-        
+        _configuration = configuration;
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration) : base(options)
+    {
+        _configuration = configuration;
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("Default"));
+
+        optionsBuilder.EnableSensitiveDataLogging();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        
+        base.OnModelCreating(modelBuilder);
     }
 }
