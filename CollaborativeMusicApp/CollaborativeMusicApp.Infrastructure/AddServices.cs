8790d9659fe124a940ce1644827b2f68515d17bd
Yeldos Manap <yeldos.manap@gmail.com>
2023-08-31 14:47:54 +0600

    Add Dependency Injection class for  Infrastructure services

---
+++ CollaborativeMusicApp/CollaborativeMusicApp.Infrastructure/AddServices.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using CollaborativeMusicApp.Application.Common;
+using CollaborativeMusicApp.Application.Contract.Persistence;
+using CollaborativeMusicApp.Infrastructure.Auth;
+using CollaborativeMusicApp.Infrastructure.Repositories;
+using CollaborativeMusicApp.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CollaborativeMusicApp.Infrastructure;
+
+public static class AddServices
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
+    {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddAuth(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SectionName, jwtSettings);
+
+        services.AddSingleton(Options.Create(typeof(JwtSettings)));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                };
+            });
+        
+        return services;
+    }
+}
